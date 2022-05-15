8cd5655d37780810c434bdb3889391f6f422e7b3
cesarocbu <cesar_8ab@hotmail.com>
2022-05-15 01:10:42 -0500

    Update v3

--- main.js
+++ main.js
@@ -1,129 +1,115 @@
-// const natalia = {
-//     name: 'natalia',
-//     age: 20,
-//     cursosAprobados: [
-//         "Curso definitivo de HTML Y CSS",
-//         "Curso practico de HTML Y CSS",
-//     ],
-
-//     aprobarCurso(nuevoCurso){
-//         this.cursosAprobados.push(nuevoCurso);
-//     },
-
-// };
-
-// //Hacer que Natalia apruebe otro curso
-
-// natalia.cursosAprobados.push("Curso de responsive design");
-
-// natalia.name = "Juan";
-
-// natalia.age += 1;
+class Course {
+    constructor({
+        name,
+        classes = [],
+    }){
+        this.name = name;
+        this.classes = classes;
+    }
+}
 
 
 
-// function Student(name, age, cursosAprobados){
-//     this.name = name;
-//     this.age = age;
-//     this.cursosAprobados = cursosAprobados;
-
-// }
-
-// Student.prototype.aprobarCurso = function(nuevoCurso){
-//     this.cursosAprobados.push(nuevoCurso);
-// }
-
-// const juanita = new Student("Juanita Alejandra", 15, ["Curso de POO", "Videojuegos"]);
-
-// //Prototipos con la sintaxis de clases
-
-// class Student2 extends Student{
-//     constructor(name, age, cursosAprobados){
-//         super(name, age, cursosAprobados);
-//         this.name = name;
-//         this.age = age;
-//         this.cursosAprobados = cursosAprobados;
-//     }
-
-//     aprobarCurso(nuevoCurso){
-//         this.cursosAprobados.push(nuevoCurso);
-//     }
-// }
-
-// const Cesar = new Student2(
-//     "Cesar Ochoa",
-//     23,
-//     ["Curso de JavaScript", "Curso practico de React"]
-// );
+const cursoProgBasica = new Course({
+    name: "Curso gratis de programación básica",
+});
+const cursoDefinitivoHTML = new Course({
+    name: "Curso definitivo de HTML y CSS",
+});
+const cursoPractiHTML = new Course({
+    name: "Curso practico de HTML y CSS",
+});
 
 
-const Juan1 = {
+class LearningPaths{
+    constructor({
+        name,
+        courses = [],
+    }) {
+        this.name = name;
+        this.courses = courses;
+    }
+}
+
+const escuelaWeb = new LearningPaths({
+    name: "Escuela de desarrollo web",
+    courses: [
+        cursoProgBasica,
+        cursoDefinitivoHTML,
+        "Curso definitivo de HTML y CCS",
+        "Curso practico de HTML y CCS"
+    ],
+});
+
+const escuelaData = new LearningPaths({
+    name: "Escuela de Data Science",
+    courses: [
+        cursoProgBasica,
+        cursoDefinitivoHTML,
+        "Curso DataBusiness",
+        "Curso Dataiz"
+    ],
+});
+
+const escuelaVgs = new LearningPaths({
+    name: "Escuela de Video Juegos",
+    courses: [
+        cursoProgBasica
+        "Curso de Unity",
+        "Curso de Unreal Engine"
+    ],
+});
+
+
+
+
+
+
+class Student {
+    constructor ({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+
+    }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+const Juan2 = new Student ({
     name: "Juan DC",
-    username: "juandc",
-    points: 100,
-    socialMedia: {
-        twitter: "fjandc",
-        instagram: "fjuandc"
-        facebook: undefined,
-    },
-    approvedCourses: [
-        "Curso definitivo de HTML y CSS",
-        "Curso practico de HTML y CSS",
-    ],
-
+    username: "Juanelodc",
+    email: "juanelodc@gmail.com"
+    twitter: "fjuandc",
     learningPaths: [
-        {
-            name: "Escuela de desarrollo web",
-            courses: [
-                "Curso definitivo de HTML y CSS",
-                "Curso practico de HTML y CSS",
-                "Curso de responsive design",
-            ]
-        },
-
-        {
-            name: "Escuela de videojuegos",
-            courses: [
-                "Curso  Introduccion a la produccion de videojuegos",
-                "Curso Unreal Engine",
-                "Curso de Unity",
-            ]
-        },
+        escuelaWeb,
+        escuelaVgs,
     ]
-};
+});
 
 
-const miguelito1 = {
-    name: "Miguel",
-    username: "miguelito",
-    points: 150,
-    socialMedia: {
-        twitter: "miguecdas",
-        instagram: "miguecdas"
-        facebook: undefined,
-    },
-    approvedCourses: [
-        "Curso definitivo de Typescript",
-        "Curso practico de Typescript",
-    ],
-
+const Cesar2 = new Student ({
+    name: "Cesar OB",
+    username: "Cesar OB",
+    email: "cesarocbu@gmail.com"
+    instagram: "@cesarocbu",
     learningPaths: [
-        {
-            name: "Escuela de desarrollo web",
-            courses: [
-                "Curso definitivo de HTML y CSS",
-                "Curso practico de HTML y CSS",
-                "Curso de responsive design",
-            ]
-        },
-
-        {
-            name: "Escuela de videojuegos",
-            courses: [
-                "Curso  Introduccion a la produccion de videojuegos",
-                "Curso Unreal Engine",
-                "Curso de Unity",
-            ]
-        },
+        escuelaWeb,
+        escuelaData,
     ]
-};
+});
+
